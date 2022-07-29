45ada225e70f76a3b229da610659a51ef41ce9c2
adermont <107684800+adermont@users.noreply.github.com>
2022-07-29 11:32:29 +0200

    Fix packaging problems
    There was conflicts between eclipse configuration and packaging configuration.
    Now the libphonenumber and emoji-java JARs are loaded as modules in the ModulePath of eclipse but they are used as regular JAR in the module-info.java.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,21 +11,19 @@ open module com.smsbr.desktop {
     requires transitive javafx.controls;
     requires transitive javafx.web;
     requires transitive javafx.swing;
-    requires jdk.jsobject;
+    requires transitive jdk.jsobject;
 
     // Other libraries dependencies
     requires org.apache.commons.io;
     requires org.apache.commons.lang3;
-    requires libphonenumber;
     requires org.jsoup;
-    requires emojijava;
-    requires json;
 
     // Public API :
     exports com.smsbr.desktop.app;
     exports com.smsbr.desktop.io;
     exports com.smsbr.desktop.model;
     exports com.smsbr.desktop.services;
+    exports com.smsbr.desktop.tools;
     exports com.smsbr.desktop.ui;
     exports com.smsbr.desktop.util;
 }
